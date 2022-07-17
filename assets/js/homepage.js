b85be87229f823c8032f5d80174fcb2bedef2e5f
Jorge Garit <jorgegarit@gmail.com>
2022-07-17 19:35:37 -0400

    added json response so that information for repositories comes back in array form and tested with my own github user

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,15 @@
 
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiURL ="https://api.github.com/users/" + user + "/repos";
+
+    // make a requets to the url
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
+
 
 getUserRepos();
