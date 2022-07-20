aea255ac924be0d1b0737e9295cf19c62a2d28f3
Jorge Garit <jorgegarit@gmail.com>
2022-07-20 13:18:10 -0400

    added display to DOM

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,24 @@
 
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a Github username");
+    }
+    
+}
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiURL ="https://api.github.com/users/" + user + "/repos";
@@ -6,10 +26,36 @@ var getUserRepos = function(user) {
     // make a requets to the url
     fetch(apiURL).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 };
 
+var displayRepos = function(repos, searchTerm) {
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
 
-getUserRepos();
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div")
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold the repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        //  append to container
+        repoEl.appendChild(titleEl);
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+
+    }
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler); 
